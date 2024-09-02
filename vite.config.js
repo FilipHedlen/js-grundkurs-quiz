0cd79d783454857d275123718b7efb550014e112
FilipHedlen <114926854+FilipHedlen@users.noreply.github.com>
2024-09-02 17:13:31 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
+import copy from 'vite-plugin-copy';
 
 export default defineConfig({
+  plugins: [
+    copy([
+      { src: 'public/_redirects', dest: '' }
+    ])
+  ],
   build: {
     outDir: 'dist',
   },
